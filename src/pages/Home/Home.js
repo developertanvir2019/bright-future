8124425c6620fff9280137d69367a603ad787a98
developertanvir2019 <developertanvir2019@gmail.com>
2023-04-30 13:31:51 +0600

    first commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h3 className='text-8xl text-blue-700'>this is home page</h3>
+        </div>
+    );
+};
+
+export default Home;
