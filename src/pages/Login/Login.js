befa82390519097a5bdb10abf20c9cadb6c3370f
developertanvir2019 <developertanvir2019@gmail.com>
2023-04-30 14:18:02 +0600

    login page done

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const styles = {
+        backgroundImage: "url('https://raw.githubusercontent.com/developertanvir2019/learnWithGame/main/public/images/logIng.png')",
+    };
     return (
-        <div>
-            this is login
+        <div className="flex h-screen w-full items-center justify-center bg-gray-100 bg-cover bg-no-repeat" style={styles}>
+            <div className="rounded-xl bg-gray-400 bg-opacity-20 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+                <div className="text-white">
+                    <div className="mb-8 flex flex-col items-center">
+                        <h1 className="mb-2 text-3xl text-white font-semibold">Login</h1>
+                    </div>
+                    <form action="#">
+                        <div className="mb-4 text-lg text-start">
+                            <label htmlFor="email"> Email</label>
+                            <input
+                                className="rounded-sm border-none bg-opacity-50 px-8 py-2 text-center text-inherit placeholder-slate-300 shadow-lg outline-none backdrop-blur-md w-full text-black"
+                                type="email"
+                                name="email"
+                                placeholder="your@email.com"
+                            />
+                        </div>
+
+                        <div className="mb-4 text-lg text-start">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                className="rounded-sm border-none bg-opacity-50 px-8 py-2 text-center text-inherit placeholder-slate-300 shadow-lg outline-none backdrop-blur-md w-full"
+                                type="password"
+                                name="password"
+                                placeholder="*********"
+                            />
+                        </div>
+                        <div className="mt-4 flex justify-center text-lg text-white font-semibold">
+                            <button
+                                type="submit"
+                                className="rounded-sm w-full btn btn-secondary mt-2"
+                            >
+                                Login
+                            </button>
+                        </div>
+                    </form>
+                    <p className='mt-2'>Don't have account? <Link className='text-green-400 font-semibold' to={'/signup'}>Create new account</Link></p>
+                </div>
+            </div>
         </div>
     );
 };
